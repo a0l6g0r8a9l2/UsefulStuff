22652656c908ad2cbfbaffcb47a765bbf307a413
Alex Gruzdev <a0l6g0r8a9l2@gmail.com>
2021-01-26 22:33:00 +0300

    upd: скрипт чтобы показать список зависимых пакетов по имени пакета

--- show_dependency_lib.py
+++ show_dependency_lib.py
@@ -1,12 +1,40 @@
 """Показать список зависимых пакетов по имени пакета"""
 import pkg_resources
+import re
 
-_package_name = 'aiogram'
-
-
-def get_dependencies_with():
-    package = pkg_resources.working_set.by_key[_package_name]
-    return [str(r) for r in package.requires()]
+_package_name = 'aiohttp'
 
 
-print(get_dependencies_with())
+def get_dependencies(_package_name):
+    try:
+        package = pkg_resources.working_set.by_key[_package_name]
+        _requires = [str(r) for r in package.requires()]
+        requires = []
+        for r in _requires:
+            substring = re.sub(r'^\w*-?\w*', '', r)
+            if substring:
+                index = r.find(substring[0])
+                # print(f'String {r}, substring {substring}, index {index}')
+                requires.append(r[:index])
+            else:
+                requires.append(_package_name)
+        return requires
+    except KeyError:
+        print(f"Не смог найти зависимые пакеты для {_package_name}, используй pip show {_package_name}")
+
+
+dependencies_lst = []
+
+
+def get_dependencies_recursive(_package_name):
+    dependencies = get_dependencies(_package_name)
+    if dependencies and len(dependencies) > 0:
+        for i in dependencies:
+            get_dependencies_recursive(i)
+    else:
+        dependencies_lst.append(_package_name)
+    return dependencies_lst
+
+
+print(get_dependencies_recursive('aiogram'))
+
